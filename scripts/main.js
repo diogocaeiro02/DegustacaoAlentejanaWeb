b8b9f60772cdf1573c2ba4a366323313281e840b
Diogo Caeiro <diogomcaeiro@gmail.com>
2025-05-03 17:47:55 +0100

    Burger Menu Implemented

--- scripts/main.js
+++ scripts/main.js
@@ -19,3 +19,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
   animatedSections.forEach((section) => observer.observe(section));
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.getElementById("burger");
+  const navMobile = document.querySelector(".nav-mobile");
+
+  burger.addEventListener("click", () => {
+    navMobile.classList.toggle("active");
+    burger.classList.toggle("active");
+  });
+
+  document.querySelectorAll(".nav-mobile a").forEach((link) => {
+    link.addEventListener("click", () => {
+      navMobile.classList.remove("active");
+      burger.classList.remove("active");
+    });
+  });
+});
